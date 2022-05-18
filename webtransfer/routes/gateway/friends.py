b7dbc134cd7b84d8162f91d12286f31bb35e47c7
iiPython <ben@iipython.cf>
2022-05-18 17:06:05 -0500

    Friends base

--- webtransfer/routes/gateway/friends.py
+++ webtransfer/routes/gateway/friends.py
@@ -12,3 +12,15 @@ def route_user_uvalidate() -> None:
         return jsonify(code = 400, message = "No users provided to validate."), 400
 
     return jsonify(code = 200, users = app.db("auth").check_users(users)), 200
+
+@app.route("/user/api/friends/request", methods = ["POST"])
+def route_user_friends_request() -> None:
+    return "500 Not Implemented", 500
+
+@app.route("/user/api/friends/accept", methods = ["POST"])
+def route_user_friends_accept() -> None:
+    return "500 Not Implemented", 500
+
+@app.route("/user/api/friends/decline", methods = ["POST"])
+def route_user_friends_decline() -> None:
+    return "500 Not Implemented", 500
--- webtransfer/routes/gateway/public.py
+++ webtransfer/routes/gateway/public.py
@@ -8,3 +8,7 @@ from flask import render_template
 @app.route("/user/dashboard", methods = ["GET"])
 def route_user_dash() -> None:
     return render_template("user/dashboard.html"), 200
+
+@app.route("/user/friends", methods = ["GET"])
+def route_user_friends() -> None:
+    return render_template("user/friends.html"), 200
